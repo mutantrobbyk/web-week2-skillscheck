5acac28e0f59bb6dc6e96cf9b2939832e3109961
Robert Knowles <robertknowles@Roberts-MacBook-Pro.local>
2019-07-04 10:26:23 -0600

    Poem

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -3,27 +3,31 @@
 
 //////////////////STEP 1////////////////////
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
-
+let lovesCode = true
 // Code Here
 
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
-
+let age = 34
 // Code Here
 
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
-
+let name = 'Rob'
 // Code Here
 
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
-
+let phrase = ''
 // Code Here
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
-
+if (lovesCode == true) {
+    phrase = 'I love code'
+} else {
+    phrase = 'I do not love to code'
+}
 // Code Here
 
 //////////////////Wrap Up////////////////////
